db31ea9278e5c32669556f7ba1dfc48906bccb4d
SerovaKaterina <k.serova.k@yandex.ru>
2024-02-26 01:38:49 +0200

    add header

--- cards/src/components/Header/Header.jsx
+++ cards/src/components/Header/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import styles from "./header.module.css";
 import imgBoy from "../../images/boy.png";
 import imgGirl from "../../images/girl.png";
+import imgVector from "../../images/Vector.png"
 
 function Header() {
     
     
     return (
         <div className= {styles.header}>
+            <img src={imgVector} alt="vector" />
             <div className={styles.header_name}><h1>ENGLISH EASY & SIMPLE</h1></div>
             <div className={styles.header_img}>
             <img src={imgBoy} alt="boy" className={styles.img_boy} />
-            <img src={imgGirl} alt="girl" className={styles.img_girl}></img>
+            <img src={imgGirl} alt="girl" className={styles.img_girl} /> 
             </div>
         </div>
     )
