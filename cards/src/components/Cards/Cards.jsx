a7516b57fdb6120349188db6839e08d5fce972d1
SerovaKaterina <k.serova.k@yandex.ru>
2024-03-29 14:00:24 +0300

    add

--- cards/src/components/Cards/Cards.jsx
+++ cards/src/components/Cards/Cards.jsx
@@ -1,55 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Cards.module.css";
 
-const Cards = ({ onSelectCategory }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
+function Cards({ onSelectCategory }) {
   const handleCategoryClick = (category) => {
     onSelectCategory(category);
-    setIsOpen(false); // Закрываем выпадающее меню после выбора категории
   };
 
   return (
-    <div className={styles.button}>
-      {isOpen ? null : (
-        <button className={styles.button_btn} onClick={toggleDropdown}>
-          Categories
-        </button>
-      )}
-      {isOpen && (
-        <div className={styles.dropdown_content}>
-          <button
-            className={styles.dropdown_btn}
-            onClick={() => handleCategoryClick("Транспорт")}
-          >
-            Transport
-          </button>
-          <button
-            className={styles.dropdown_btn}
-            onClick={() => handleCategoryClick("Одежда")}
-          >
-            Clothes
-          </button>
-          <button
-            className={styles.dropdown_btn}
-            onClick={() => handleCategoryClick(" Игрушки")}
-          >
-            Toys
-          </button>
-          <button
-            className={styles.dropdown_btn}
-            onClick={() => handleCategoryClick("Семья")}
-          >
-            Family
-          </button>
-        </div>
-      )}
+    <div className={styles.dropdown_content}>
+      <button
+        className={styles.dropdown_btn}
+        onClick={() => handleCategoryClick("Транспорт")}
+      >
+        Transport
+      </button>
+      <button
+        className={styles.dropdown_btn}
+        onClick={() => handleCategoryClick("Одежда")}
+      >
+        Clothes
+      </button>
+      <button
+        className={styles.dropdown_btn}
+        onClick={() => handleCategoryClick("Игрушки")}
+      >
+        Toys
+      </button>
+      <button
+        className={styles.dropdown_btn}
+        onClick={() => handleCategoryClick("Семья")}
+      >
+        Family
+      </button>
     </div>
   );
-};
+}
 
 export default Cards;
--- cards/src/components/Carousel/Carousel.jsx
+++ cards/src/components/Carousel/Carousel.jsx
@@ -7,6 +7,7 @@ import nextImg from '../.././images/pngegg2.png'
 
 function Carousel({ items, category }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [pressed, setPressed] = useState(Array(items.length).fill(false));
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex === items.length - 1 ? 0 : prevIndex + 1));
@@ -16,32 +17,46 @@ function Carousel({ items, category }) {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? items.length - 1 : prevIndex - 1));
   };
 
+  const handleClick = (index) => {
+    setPressed((prevState) => {
+      const newPressed = [...prevState];
+      newPressed[index] = !newPressed[index];
+      return newPressed;
+    });
+  };
+
   return (
     <div className={styles.carousel}>
-      <button className={styles.prev} onClick={prevSlide}><img src={prevImg} alt='prev'></img></button>
+      <button className={styles.prev} onClick={prevSlide}><img src={prevImg} alt='prev' /></button>
       {items.map((item, index) => (
         <div
           key={index}
           className={index === currentIndex ? styles.active : styles.inactive}
         >
-          {category === 'Транспорт' ? (
-               <TransportCard
-               url={item.url}
-               english={item.english}
-               transcription={item.transcription}
-               russian={item.russian}
-             />
-          ) : (
-            <ClothesCard
-              url={item.url}
-              english={item.english}
-              transcription={item.transcription}
-              russian={item.russian}
-            />
-          )}
+         {category === 'Транспорт' ? (
+  <TransportCard
+    url={item.url}
+    english={item.english}
+    transcription={item.transcription}
+    russian={pressed[index] ? item.russian : (
+      <button className={styles.translate} onClick={() => handleClick(index)}>Перевести</button>
+    )}
+    pressed={pressed[index]}
+    handleClick={() => handleClick(index)}
+  />
+) : (
+  <ClothesCard
+    url={item.url}
+    english={item.english}
+    transcription={item.transcription}
+    russian={pressed[index] ? item.russian : (
+      <button className={styles.translate} onClick={() => handleClick(index)}>Посмотреть перевод</button>
+    )}
+  />
+)}
         </div>
       ))}
-      <button className={styles.next} onClick={nextSlide}><img src={nextImg} alt='next'></img></button>
+      <button className={styles.next} onClick={nextSlide}><img src={nextImg} alt='next' /></button>
     </div>
   );
 }
