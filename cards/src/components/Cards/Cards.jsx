3ff37c2b283100d8cd95ff694983e35b7d88f447
SerovaKaterina <k.serova.k@yandex.ru>
2024-04-02 23:13:30 +0300

    add styles

--- cards/src/components/Cards/Cards.jsx
+++ cards/src/components/Cards/Cards.jsx
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Cards.module.css";
 
 function Cards({ onSelectCategory }) {
-  //обработчик клика по категории
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
   const handleCategoryClick = (category) => {
     onSelectCategory(category);
+    setSelectedCategory(category); // Установка выбранной категории
   };
 
   return (
     <div className={styles.dropdown_content}>
       <button
-        className={styles.dropdown_btn}
+        className={`${styles.dropdown_btn} ${selectedCategory === "Транспорт" ? styles.selected : ""}`}
         onClick={() => handleCategoryClick("Транспорт")}
       >
         Transport
       </button>
       <button
-        className={styles.dropdown_btn}
+        className={`${styles.dropdown_btn} ${selectedCategory === "Одежда" ? styles.selected : ""}`}
         onClick={() => handleCategoryClick("Одежда")}
       >
         Clothes
       </button>
       <button
-        className={styles.dropdown_btn}
+        className={`${styles.dropdown_btn} ${selectedCategory === "Игрушки" ? styles.selected : ""}`}
         onClick={() => handleCategoryClick("Игрушки")}
       >
         Toys
       </button>
       <button
-        className={styles.dropdown_btn}
+        className={`${styles.dropdown_btn} ${selectedCategory === "Семья" ? styles.selected : ""}`}
         onClick={() => handleCategoryClick("Семья")}
       >
         Family
