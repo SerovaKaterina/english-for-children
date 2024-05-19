a5c531fae9569b18ddf48e40ff894ce9e24cf273
SerovaKaterina <k.serova.k@yandex.ru>
2024-05-19 23:58:15 +0300

    add changes

--- cards/src/components/NavBar/NavBar.jsx
+++ cards/src/components/NavBar/NavBar.jsx
@@ -7,13 +7,11 @@ function NavBar({ onTableClick }) {
   return (
     <div>
       <ul className={styles.links}>
-        {/* ссылка на таблицу */}
         <li>
           <Link to="/table" onClick={onTableClick} className={styles.links1}>
             Table
           </Link>
         </li>
-        {/* ссылка на карточки */}
         <li>
           <Link to="/cards" className={styles.links1}>Cards</Link>
         </li>
